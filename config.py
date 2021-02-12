5d2ca463ac0fe1ef7fc9b2556d3a0e50c932635b
ejbosia <ejbosia@gmail.com>
2021-02-12 11:28:07 -0500

    Update "allowed extensions"

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess_password'
 
     ALLOWED_EXTENSIONS = [
-        ".gcode"
+        "gcode"
     ]
 
     UPLOAD_FOLDER = "files"
