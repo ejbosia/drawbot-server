7a6a61d9d7ead9c6d885ab79fd85e5410783e9f6
ejbosia <ejbosia@gmail.com>
2021-02-12 10:30:52 -0500

    Set the host to 0.0.0.0
    To allow open communication

--- drawbot_server.py
+++ drawbot_server.py
@@ -3,4 +3,5 @@ from app import app
 
 if __name__ == "__main__":
     # app.run(debug=True, host='192.168.1.76')
-    app.run(debug=True, host='127.0.0.1')
+    # app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='0.0.0.0')
